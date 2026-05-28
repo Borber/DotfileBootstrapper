42b2e3c698a58a1376e737bc4603c6392a045682
Borber <borber.drum@gmail.com>
2026-05-29 02:10:17 +0800

    feat(zen): add rose-pine themes and user preferences

---
+++ items/zen/src/user.js
@@ -0,0 +1,3 @@
+user_pref("font.name-list.emoji", "Noto Color Emoji");
+user_pref("font.name.sans-serif.zh-CN", "LXGW Bright");
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
